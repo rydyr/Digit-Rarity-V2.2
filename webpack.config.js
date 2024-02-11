9a17945b5f4f71eb00cf0a8392726806f2c7e385
rydyr <matthewmedmiston@gmail.com>
2024-02-11 16:27:47 -0500

    Initial Commit - Ground Zero Build // React - Jest - Webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+   /* experiments: {
+        outputModule: true,
+    },*/
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env','@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            }
+        ]
+    },
+    devServer: {
+        static: './dist',
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+    },
+};
