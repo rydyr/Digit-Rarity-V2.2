77be3df884c261ad3262289b3ab29fb8bd92e7c0
rydyr <matthewmedmiston@gmail.com>
2024-02-29 19:04:13 -0500

    moved owner area over

--- src/components/owner.jsx
+++ src/components/owner.jsx
@@ -11,11 +11,11 @@ export default function Owner(props) {
         <div className="ownerArea" style={style} >
             <p id="ownerTag">Owner: {props.owner}</p>
             <a href={ensName} target="_blank" rel="noopener noreferrer" id="vLink">
-                <span className="colorBlack">~</span>
+                <span className="colorBlack">~ </span>
                 <span className="colorYellow">Vision</span>
                 <span className="colorBlack">.</span>
                 <span className="colorYellow">io</span>
-                <span className="colorBlack">~</span> 
+                <span className="colorBlack"> ~</span> 
             </a>
         </div>
     )
